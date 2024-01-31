0d93f29e8c90ebfc99aa6ebb763b02194d14910e
AlessiaOsman <alessia.osman@gmail.com>
2024-01-31 16:59:31 +0100

    added component apphero

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <script>
-import AppTitle from './components/AppTitle.vue'
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   name: 'HelloVite',
   
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 };
 </script>
 
 <template>
-  <AppTitle />
-</template>
+  <div class="container">
+    <AppTitle />
+    <AppHero/>
+  </div>
+  </template>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .container{
+    margin: 24px auto;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+</style>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    name: 'AppHero',
+    data: () => ({
+        image: 'landscape.jpg'
+    }),
+    methods: {
+        createImagePath(image){
+            const url = new URL (`../assets/img/${image}`, import.meta.url);
+            return url.href
+
+        }
+    }
+  };
+</script>
+
+<template>
+ <img :src="createImagePath(image)" alt="">
+</template>
+
+<style>
+    img{
+        margin: 0 auto;
+        width: 90%;
+    }
+</style>
