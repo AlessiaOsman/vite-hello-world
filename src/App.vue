3f388b3686ec108f9b543d292a3493830c82401b
AlessiaOsman <alessia.osman@gmail.com>
2024-01-31 16:10:38 +0100

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script>
-  export default {
-    name: 'HelloVite'
-  };
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: 'HelloVite',
+  
+  components: {
+    AppTitle
+  }
+};
 </script>
 
 <template>
-  
+  <AppTitle />
 </template>
 
-<style>
-
-</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'AppTitle',
+    data: () => ({
+        text: 'La mia prima app con Vite!'
+    }),
+  };
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<style>
+    h1{
+        color: blueviolet;
+        text-align: center;
+    }
+</style>
